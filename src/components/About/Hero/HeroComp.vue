ae6b24ade405d3ea11dc69223c79d3023e671307
AkmalovAbduvoris <akmalovabduvoris@gmail.com>
2023-05-29 12:31:37 +0500

    Heroo

--- src/components/About/Hero/HeroComp.vue
+++ src/components/About/Hero/HeroComp.vue
@@ -2,18 +2,18 @@
 import './Hero.css'
 </script>
 <template>
-    <section class="hero">
+    <section class="heroo">
         <div class="container">
             <div class="heroo__wrapper">
-                <div class="hero__right-wrapper">
-                <p class="hero__right-text">Любая категория</p>
-                <p class="hero__center-text">Что будем искать ?</p>
-                <p class="hero__left-text">По всей Узбекистану</p>
-                <button class="hero__right-btn">Найти</button>
+                <div class="heroo__right-wrapper">
+                <p class="heroo__right-text">Любая категория</p>
+                <p class="heroo__center-text">Что будем искать ?</p>
+                <p class="heroo__left-text">По всей Узбекистану</p>
+                <button class="heroo__right-btn">Найти</button>
                 </div>
-                <div class="hero__left__wrapper">
+                <div class="heroo__left__wrapper">
                     <router-Link to="/add">
-                        <button class="hero__left-btn">Добавить обьявления</button>
+                        <button class="heroo__left-btn">Добавить обьявления</button>
                     </router-Link>
                 </div>
             </div>
