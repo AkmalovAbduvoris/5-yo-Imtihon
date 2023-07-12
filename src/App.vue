5d1d7d76158e16110b7f8efd0110d7e780d1c72d
AkmalovAbduvoris <akmalovabduvoris@gmail.com>
2023-07-12 12:15:36 +0500

    ok

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import { useRouter } from 'vue-router';
 const router = useRouter()
 let token = window.localStorage.getItem('token')
 if(!token){
-    location.href = '/'
+    router.push('/login')
+    // location.href = '/'
 }
 </script>
 
